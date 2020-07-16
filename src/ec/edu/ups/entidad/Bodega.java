d71714a5fa899e596ac92f3002557184856ff2be
aReinoso007 <ralexjessiel@gmail.com>
2020-07-15 22:03:02 -0500

    arreglado auto increment
    no se generaba el autoincrment, ya esta solucionado

--- src/ec/edu/ups/entidad/Bodega.java
+++ src/ec/edu/ups/entidad/Bodega.java
@@ -15,7 +15,7 @@ public class Bodega implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int codigoBodega;
 
 	private String nombre;
