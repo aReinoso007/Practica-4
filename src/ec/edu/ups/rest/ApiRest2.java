d9a2dd362ef16ac0b15e1cff374ba2670356ad42
aReinoso007 <ralexjessiel@gmail.com>
2020-07-17 00:07:04 -0500

    rest listar usuarios

--- src/ec/edu/ups/rest/ApiRest2.java
+++ src/ec/edu/ups/rest/ApiRest2.java
@@ -1,7 +1,16 @@
 package ec.edu.ups.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ejb.EJB;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import ec.edu.ups.ejb.RolFacade;
 import ec.edu.ups.ejb.UsuarioFacade;
@@ -16,6 +25,20 @@ public class ApiRest2 {
 	private RolFacade ejbRolFacade;
 	
 	private Usuario usuario;
+	
+	@GET
+	@Path("/listadousuarios")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response listaUsuarios() {
+		List<Usuario> listaUsuarios = new ArrayList<Usuario>();
+		Jsonb jsonb = JsonbBuilder.create();
+		listaUsuarios = ejbUsuarioFacade.findAll();
+		System.out.println("retornando lista de usuarios");
+		return Response.ok(jsonb.toJson(listaUsuarios))
+				.header("Access-Control-Allow-Origin", "*")
+				.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+				.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE").build();
+	}
 	
 
 }
